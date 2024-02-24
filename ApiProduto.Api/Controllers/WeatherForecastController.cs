8af1288fff7c333b8bd3ddae79929735938babee
Antonio <antonioocarlos202@gmail.com>
2024-02-24 15:36:27 -0300

    Adicionar arquivos de projeto.

---
+++ ApiProduto.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProduto.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+      
+    }
+}
---
+++ ApiProduto/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProduto.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+    }
+}
